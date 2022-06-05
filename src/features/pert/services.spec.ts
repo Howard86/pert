73879731f922770aaa82414d2fa665602d8b47b6
Howard Tai <howard@howardism.dev>
2022-06-05 16:05:49 +0800

    test(pert): add service mock with spy on localstorage

--- src/features/pert/services.spec.ts
+++ src/features/pert/services.spec.ts
@@ -1,7 +1,39 @@
-import './services';
+import { importFromLocalStorage, saveToLocalStorage } from './services';
 
 describe('services', () => {
-  it.todo('should call local storage saveItem');
+  it('should call local storage setItem', () => {
+    expect.hasAssertions();
+    jest.spyOn(Storage.prototype, 'setItem');
+    saveToLocalStorage({
+      unit: 'unit',
+      tasks: [
+        {
+          name: 'TEST',
+          optimisticEstimate: 0,
+          likelyEstimate: 0,
+          pessimisticEstimate: 0,
+        },
+      ],
+    });
 
-  it.todo('should call local storage getItem');
+    expect(window.localStorage.setItem).toHaveBeenCalledTimes(1);
+  });
+
+  it('should call local storage getItem', () => {
+    expect.hasAssertions();
+    jest.spyOn(Storage.prototype, 'getItem');
+    importFromLocalStorage({
+      unit: 'unit',
+      tasks: [
+        {
+          name: 'TEST',
+          optimisticEstimate: 0,
+          likelyEstimate: 0,
+          pessimisticEstimate: 0,
+        },
+      ],
+    });
+
+    expect(window.localStorage.getItem).toHaveBeenCalledTimes(1);
+  });
 });
